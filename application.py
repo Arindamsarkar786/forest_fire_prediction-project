3e352d2e85c59ccec9dac9ca627c0f105f6a3911
Arindamsarkar786 <arindam8345075860@gmail.com>
2023-06-27 16:05:40 +0530

    first commit

--- application.py
+++ application.py
@@ -16,9 +16,8 @@ standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 def index():
     return render_template('index.html')
 
-
 @app.route('/predictdata',methods=['GET','POST'])
-def predict_data():
+def predict_datapoint():
     if request.method=='POST':
         Temperature=float(request.form.get('Temperature'))
         RH = float(request.form.get('RH'))
@@ -30,10 +29,11 @@ def predict_data():
         Classes = float(request.form.get('Classes'))
         Region = float(request.form.get('Region'))
 
-        new_scaled_data = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result=ridge_model.predict(new_scaled_data)
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
 
         return render_template('home.html',result=result[0])
+
     else:
         return render_template('home.html')
 
